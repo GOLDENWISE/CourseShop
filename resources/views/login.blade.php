9b797d938378b54187067db8468b5ab37f8b50ae
GOLDENWISE <abdamadhafiz13@gmail.com>
2023-12-18 11:18:06 +0700

    memperbaiki penulisan kode pada halaman login dan register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.auth')
 @section('authorization-content')
 <section class="login" style="height: 476px;">
-      <h1>LOGIN</h1>
-      <form action="">
-        @csrf
-        <input type="email" name="email" id="email" placeholder="Email">
-        <input type="password" name="password" id="password" placeholder="Password">
-        <button type="submit" name="register">SUBMIT</button>
-      </form>
-      <p>Belum memiliki akun? Lakukan <a href="/register">register</a> sekarang!</p>
-    </section>
-    @endsection
+  <h1>LOGIN</h1>
+  <form action="">
+    @csrf
+    <input type="email" name="email" id="email" placeholder="Email">
+    <input type="password" name="password" id="password" placeholder="Password">
+    <button type="submit" name="register">SUBMIT</button>
+  </form>
+  <p>Belum memiliki akun? Lakukan <a href="/register">register</a> sekarang!</p>
+</section>
+@endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.auth')
 @section('authorization-content')
 <section class="register" style="height: 601px;">
-      <h1>REGISTER</h1>
-      <form action="">
-        <input type="text" name="username" id="username" placeholder="Username">
-        <input type="email" name="email" id="email" placeholder="Email">
-        <input type="password" name="password" id="password" placeholder="Password">
-        <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
-        <button type="submit" name="register">SUBMIT</button>
-      </form>
-      <p>Sudah memiliki akun? Lakukan <a href="/login">login</a> sekarang!</p>
-    </section>
-  </section>
-</body>
-</html>
+  <h1>REGISTER</h1>
+  <form action="">
+    <input type="text" name="username" id="username" placeholder="Username">
+    <input type="email" name="email" id="email" placeholder="Email">
+    <input type="password" name="password" id="password" placeholder="Password">
+    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
+    <button type="submit" name="register">SUBMIT</button>
+  </form>
+  <p>Sudah memiliki akun? Lakukan <a href="/login">login</a> sekarang!</p>
+</section>
 @endsection
